18e326444a3b57df7208f4e55fcd2814d2d017a9
2 Train Robotics <157223793+2trainrobotics@users.noreply.github.com>
2025-10-02 18:31:56 -0400

    New components added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopDecode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopDecode.java
@@ -2,17 +2,39 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TeleopDecode", group = "FTC")
 public class TeleopDecode extends LinearOpMode {
 
-    // "Here's where we declare all of the wheels' motors
+    // Here's where we declare all of the wheels' motors
 
     private DcMotor leftFrontDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor leftRearDrive = null;
     private DcMotor rightRearDrive = null;
+
+    // Here's where we declare the operational motors:
+
+    private DcMotor wilmerClimberLeft = null;
+    private DcMotor jhoandryClimberRight = null;
+    private DcMotor shooter = null;
+
+
+    // Here's where we declare the operational Servos
+
+    private CRServo intake = null;
+    private CRServo secondIntake = null;
+
+    // Here we declare the motors integer positions
+
+    final int HOME_POSITION = 10;
+    final int PARK_POSITION = 2000;
+
+    int wilmerPosition = HOME_POSITION;
+    int jhoandryPosition = HOME_POSITION;
 
     @Override
     public void runOpMode () {
@@ -25,6 +47,15 @@ public class TeleopDecode extends LinearOpMode {
         leftRearDrive = hardwareMap.get(DcMotor.class,"leftRearDrive");
         rightFrontDrive = hardwareMap.get(DcMotor.class,"rightFrontDrive");
         rightRearDrive = hardwareMap.get(DcMotor.class,"rightRearDrive");
+        wilmerClimberLeft = hardwareMap.get(DcMotor.class,"wilmerClimberLeft");
+        jhoandryClimberRight = hardwareMap.get(DcMotor.class,"jhoandryClimbeRight");
+        shooter = hardwareMap.get(DcMotor.class,"shooter");
+
+        // We'd be doing the same thing here, but for the servos:
+
+        intake = hardwareMap.get(CRServo.class,"intake");
+        secondIntake = hardwareMap.get(CRServo.class,"secondIntake");
+
 
         /* Here's where we set the directions of the wheels
         to makes sure that all of them are going forward.
@@ -33,7 +64,30 @@ public class TeleopDecode extends LinearOpMode {
         leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightRearDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightRearDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Here we set directions for the parking climbers:
+
+        wilmerClimberLeft.setDirection(DcMotor.Direction.FORWARD);
+        jhoandryClimberRight.setDirection(DcMotor.Direction.FORWARD);
+
+        // Here we set the directions for the intakes:
+
+        intake.setDirection(CRServo.Direction.FORWARD);
+        secondIntake.setDirection(CRServo.Direction.FORWARD);
+
+        // Here's where we configure the Zero Power Behavior for the Climbers:
+
+        wilmerClimberLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        jhoandryClimberRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Here we program the climbers' encoders:
+
+        wilmerClimberLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wilmerClimberLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        jhoandryClimberRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        jhoandryClimberRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
 
@@ -62,6 +116,14 @@ public class TeleopDecode extends LinearOpMode {
             leftRearDrive.setPower(leftRearPower);
             rightFrontDrive.setPower(rightFrontPower);
             rightRearDrive.setPower(rightRearPower);
+
+            // Here's where we set the commands for all of the operational components:
+
+            if(gamepad1.a) {
+                intake.setPower(1);
+            }
+
+
         }
     }
 }
